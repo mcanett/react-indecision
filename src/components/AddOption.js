105f7facf7f4b6b4dc624eb10e048e92d2df0f9a
Moisés Canett <moises.canett@gmail.com>
2018-07-05 14:59:09 -0700

    OptionModal styled AddOption style fixed favicon added Mobile consideration added

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -8,7 +8,6 @@ export default class AddOption extends React.Component {
 
 	handleAddOption = (e) => {
 		e.preventDefault();
-		console.log('testing');
 		const option = e.target.elements.option.value.trim();
 		const error = this.props.handleAddOption(option);
 
